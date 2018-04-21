a507e529a930914cdd41ab847dd091c23b5d2a04
JAYESHKORI <jayeshmkori@gmail.com>
2018-04-21 20:38:09 +0530

    Permission is issue resolved

--- app/src/main/java/com/example/jayesh/ghostel/Activity/MainActivity.java
+++ app/src/main/java/com/example/jayesh/ghostel/Activity/MainActivity.java
@@ -1,42 +1,117 @@
 package com.example.jayesh.ghostel.Activity;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.jayesh.ghostel.R;
 import com.example.jayesh.ghostel.SharedPrefrences.Session;
+import com.karan.churi.PermissionManager.PermissionManager;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tv_granted,tv_denied;
     Session session;
+
+    PermissionManager permissionManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         session = new Session(MainActivity.this);
-        Log.d("id",String.valueOf(session.getid()));
-        if(session.getid()!=0)
+        tv_granted = (TextView)findViewById(R.id.tv_granted);
+        tv_denied = (TextView)findViewById(R.id.tv_denied);
+
+        permissionManager = new PermissionManager() {};
+        if(permissionManager.checkAndRequestPermissions(this))
         {
-            if (session.getUsertype().charAt(0)=='S'){
-                startActivity(new Intent(MainActivity.this,StudentActivity.class));finish();
+            if(session.getid()!=0)
+            {
+                if (session.getUsertype().charAt(0)=='S'){
+                    startActivity(new Intent(MainActivity.this,StudentActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='R'){
+                    startActivity(new Intent(MainActivity.this,RectorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='C'){
+                    startActivity(new Intent(MainActivity.this,ContractorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='A'){
+                    startActivity(new Intent(MainActivity.this,AdminActivity.class));finish();
+                }
             }
-            if (session.getUsertype().charAt(0)=='R'){
-                startActivity(new Intent(MainActivity.this,RectorActivity.class));finish();
-            }
-            if (session.getUsertype().charAt(0)=='C'){
-                startActivity(new Intent(MainActivity.this,ContractorActivity.class));finish();
-            }
-            if (session.getUsertype().charAt(0)=='A'){
-                startActivity(new Intent(MainActivity.this,AdminActivity.class));finish();
+            else
+            {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));finish();
             }
         }
-        else
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        permissionManager.checkResult(requestCode,permissions,grantResults);
+        ArrayList<String> granted = permissionManager.getStatus().get(0).granted;
+        ArrayList<String> denied = permissionManager.getStatus().get(0).denied;
+
+        if(granted.size()==4)
         {
-            startActivity(new Intent(MainActivity.this,LoginActivity.class));
-            finish();
+            if(session.getid()!=0)
+            {
+                if (session.getUsertype().charAt(0)=='S'){
+                    startActivity(new Intent(MainActivity.this,StudentActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='R'){
+                    startActivity(new Intent(MainActivity.this,RectorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='C'){
+                    startActivity(new Intent(MainActivity.this,ContractorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='A'){
+                    startActivity(new Intent(MainActivity.this,AdminActivity.class));finish();
+                }
+            }
+            else
+            {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));finish();
+            }
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        if(permissionManager.checkAndRequestPermissions(this))
+        {
+            if(session.getid()!=0)
+            {
+                if (session.getUsertype().charAt(0)=='S'){
+                    startActivity(new Intent(MainActivity.this,StudentActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='R'){
+                    startActivity(new Intent(MainActivity.this,RectorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='C'){
+                    startActivity(new Intent(MainActivity.this,ContractorActivity.class));finish();
+                }
+                if (session.getUsertype().charAt(0)=='A'){
+                    startActivity(new Intent(MainActivity.this,AdminActivity.class));finish();
+                }
+            }
+            else
+            {
+                startActivity(new Intent(MainActivity.this,LoginActivity.class));finish();
+            }
         }
     }
 }
